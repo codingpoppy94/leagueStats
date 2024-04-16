855b0ff13f1b0d0bfec7469db90e2b350529e561
junghyun <yly0617@metabuild.co.kr>
2024-04-16 18:07:46 +0900

    0416수정

--- src/main/java/com/stats/lolgg/bot/ReadyListener.java
+++ src/main/java/com/stats/lolgg/bot/ReadyListener.java
@@ -28,94 +28,118 @@ public class ReadyListener extends ListenerAdapter {
     
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        // 테스트1
-        if(event.getMessage().getContentRaw().equals("ping")){
-            log.info("message get");
-            event.getChannel().sendMessage("pong!").queue();
-            return;
-        }
+        
+        String message = event.getMessage().getContentRaw();
 
-        // 목록 추가
-        if(event.getMessage().getContentRaw().equals("!ㅊㅋ")){
-            log.info("목록 추가");
-            userManager.addUser(event.getMember());
-            MessageChannel channel = event.getChannel();
-
-            channel.sendMessage("Users: \n" + userManager.sendUserList() ).queue();            
-        }
-
-        // 목록 삭제
-        if (event.getMessage().getContentRaw().equals("!ㅊㅅ")) {
-            log.info("목록 삭제");
-            Member user = event.getMember();
-            boolean removed = userManager.removeUser(user);
-            MessageChannel channel = event.getChannel();
-
-            if (removed) {
-                channel.sendMessage("User " + user.getNickname() + " removed.\nUsers: \n" + userManager.sendUserList() ).queue();
-            } else {
-                channel.sendMessage("User " + user.getNickname() + " not found in the list.").queue();
-            }
-        }
-
-        // 목록 보기
-        if (event.getMessage().getContentRaw().equals("!ㄷㄱ")) {
-            log.info("목록 보기");
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("Users: \n" + userManager.sendUserList()).queue();
-        }
-
-
-        String[] message = event.getMessage().getContentRaw().split("\\s+");
-        // 메시지 예시 !ㅁㅅ {1~4} {메시지}
-        // "!ㅁㅅ1" 명령어를 받았을 때
-        if (message[0].equalsIgnoreCase("!ㅁㅅ")) {
-            if (message.length < 2) {
-                event.getChannel().sendMessage("메시지를 입력하세요.").queue();
+        if(message.chatAt(0) == '!'){
+            
+            // 테스트1
+            if(message.equals("!ping")){
+                log.info("message get");
+                event.getChannel().sendMessage("pong!").queue();
                 return;
             }
-
-            Pattern pattern = Pattern.compile("!ㅁㅅ\\s+(\\d{1,2})~(\\d{1,2})\\s+(.+)");
-            Matcher matcher = pattern.matcher(event.getMessage().getContentRaw());
-
-            int minMentionCount = Integer.parseInt(matcher.group(1)); // 최소 멘션할 사용자 수
-            int maxMentionCount = Integer.parseInt(matcher.group(2)); // 최대 멘션할 사용자 수
-            String welcomeMessage = matcher.group(3); // 환영 메시지
-
-            List<Member> userList = userManager.getUserList();
-
-            if (userList.isEmpty()) {
-                event.getChannel().sendMessage("멤버 리스트가 비어있습니다.").queue();
-                return;
+    
+            // 목록 추가
+            if(message.equals("!ㅊㅋ")){
+                log.info("목록 추가");
+                userManager.addUser(event.getMember());
+                MessageChannel channel = event.getChannel();
+    
+                channel.sendMessage("Users: \n" + userManager.sendUserList() ).queue();            
+            }
+    
+            // 목록 삭제
+            if (message.equals("!ㅊㅅ")) {
+                log.info("목록 삭제");
+                Member user = event.getMember();
+                boolean removed = userManager.removeUser(user);
+                MessageChannel channel = event.getChannel();
+    
+                if (removed) {
+                    channel.sendMessage("User " + user.getNickname() + " removed.\nUsers: \n" + userManager.sendUserList() ).queue();
+                } else {
+                    channel.sendMessage("User " + user.getNickname() + " not found in the list.").queue();
+                }
+            }
+    
+            // 목록 보기
+            if (message.equals("!ㄷㄱ")) {
+                log.info("목록 보기");
+                MessageChannel channel = event.getChannel();
+                channel.sendMessage("Users: \n" + userManager.sendUserList()).queue();
+            }
+    
+    
+            String[] message = event.getMessage().getContentRaw().split("\\s+");
+            // 메시지 예시 !ㅁㅅ {1~4} {메시지}
+            // "!ㅁㅅ1" 명령어를 받았을 때
+            if (message[0].equalsIgnoreCase("!ㅁㅅ")) {
+                if (message.length < 2) {
+                    event.getChannel().sendMessage("메시지를 입력하세요.").queue();
+                    return;
+                }
+    
+                Pattern pattern = Pattern.compile("!ㅁㅅ\\s+(\\d{1,2})~(\\d{1,2})\\s+(.+)");
+                Matcher matcher = pattern.matcher(event.getMessage().getContentRaw());
+    
+                int minMentionCount = Integer.parseInt(matcher.group(1)); // 최소 멘션할 사용자 수
+                int maxMentionCount = Integer.parseInt(matcher.group(2)); // 최대 멘션할 사용자 수
+                String welcomeMessage = matcher.group(3); // 환영 메시지
+    
+                List<Member> userList = userManager.getUserList();
+    
+                if (userList.isEmpty()) {
+                    event.getChannel().sendMessage("멤버 리스트가 비어있습니다.").queue();
+                    return;
+                }
+    
+                if (maxMentionCount > userList.size()) {
+                    event.getChannel().sendMessage("대기 인원을 확인하세요.").queue();
+                    return;
+                }
+    
+                // // 리스트에서 첫 번째 멤버를 얻어옵니다.
+                // Member firstMember = event.getMember();
+                // log.info(firstMember.getAvatarId());
+    
+                for(int i=minMentionCount; i<maxMentionCount; i++){  
+                    Member mentionMember = userList.get(i);
+                    event.getChannel().sendMessage(mentionMember.getAsMention());
+                }
+    
+                // 해당 멤버에게 멘션을 보냅니다.
+                // {메시지 부분 삽입}
+                event.getChannel().sendMessage(welcomeMessage).queue();
+            }
+    
+            if (event.getMessage().getContentRaw().equals("!서버")) {
+                // JDA jda = event.getJDA();
+                Guild guild = event.getGuild();
+                String servierId = guild.getId();
+                log.info("serverId " + servierId);
+    
+                MessageChannel channel = event.getChannel();
+                channel.sendMessage("서버 ID: " + servierId).queue();
             }
 
-            if (maxMentionCount > userList.size()) {
-                event.getChannel().sendMessage("대기 인원을 확인하세요.").queue();
-                return;
-            }
 
-            // // 리스트에서 첫 번째 멤버를 얻어옵니다.
-            // Member firstMember = event.getMember();
-            // log.info(firstMember.getAvatarId());
 
-            for(int i=minMentionCount; i<maxMentionCount; i++){  
-                Member mentionMember = userList.get(i);
-                event.getChannel().sendMessage(mentionMember.getAsMention());
-            }
 
-            // 해당 멤버에게 멘션을 보냅니다.
-            // {메시지 부분 삽입}
-            event.getChannel().sendMessage(welcomeMessage).queue();
-        }
 
-        if (event.getMessage().getContentRaw().equals("!서버")) {
-            // JDA jda = event.getJDA();
-            Guild guild = event.getGuild();
-            String servierId = guild.getId();
-            log.info("serverId " + servierId);
 
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("서버 ID: " + servierId).queue();
+
+
+
+
+
+
+
+
+
+
+
+
         }
 
 
