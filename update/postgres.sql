cf90e66909c6d49677823a93e3ea71f0863590c3
junghyun <yly0617@metabuild.co.kr>
2024-04-24 15:57:41 +0900

    수정된 table script 반영

--- update/postgres.sql
+++ update/postgres.sql
@@ -9,7 +9,7 @@ CREATE TABLE league (
     champ_name VARCHAR(200),
     position VARCHAR(100),
     kda VARCHAR(20),
-    game_result boolean not null,
+    game_result VARCHAR(20),
     game_team VARCHAR(20),
     game_date TIMESTAMP,
     create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -18,4 +18,4 @@ CREATE TABLE league (
 
 INSERT INTO public.league
 (id, game_id, riot_name, champ_name, position, kda, game_result, game_team, game_date, create_date, delete_yn)
-VALUES('1', '03-28-1-1', '잘정', '자르반4세', 'JUG', '10/1/13', true, 'blue', '20240328', CURRENT_TIMESTAMP, 'N');
+VALUES('1', '03-28-1-1', '잘정', '자르반4세', 'JUG', '10/1/13', "승", 'blue', '20240328', CURRENT_TIMESTAMP, 'N');
