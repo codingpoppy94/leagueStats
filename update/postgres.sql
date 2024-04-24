32853a1d5f229cb10974629be5fdf8cb6284550e
junghyun <yly0617@metabuild.co.kr>
2024-04-24 18:05:33 +0900

    feat: 조회관련 sql 추가

--- update/postgres.sql
+++ update/postgres.sql
@@ -19,3 +19,55 @@ CREATE TABLE league (
 INSERT INTO public.league
 (id, game_id, riot_name, champ_name, position, kda, game_result, game_team, game_date, create_date, delete_yn)
 VALUES('1', '03-28-1-1', '잘정', '자르반4세', 'JUG', '10/1/13', "승", 'blue', '20240328', CURRENT_TIMESTAMP, 'N');
+
+
+-- 최근전적 10 조회
+SELECT id, game_id, riot_name, champ_name, position, kda, game_result, game_team, game_date, create_date
+FROM league
+
+where riot_name = '잘생긴정현이'
+and delete_yn = 'N'
+and game_date < current_timestamp 
+ORDER BY ABS(EXTRACT(EPOCH FROM CURRENT_TIMESTAMP - game_date))
+LIMIT 10;
+
+-- 전체 조회
+SELECT id, game_id, riot_name, champ_name, position, kda, game_result, game_team, game_date, create_date
+FROM league
+
+where riot_name = '잘생긴정현이'
+and delete_yn = 'N';
+
+-- 이번달 조회
+SELECT id, game_id, riot_name, champ_name, position, kda, game_result, game_team, game_date, create_date
+FROM league
+where riot_name = '잘생긴정현이'
+and delete_yn  = 'N'
+and game_date >= DATE_TRUNC('month', CURRENT_TIMESTAMP)
+and game_date < DATE_TRUNC('month', CURRENT_TIMESTAMP) + INTERVAL '1 month';
+
+-- 전적 count
+select
+	position ,
+    COUNT(*) AS total_count,
+    COUNT(CASE WHEN game_result = '승' THEN 1 END) AS win,
+    COUNT(CASE WHEN game_result = '패' THEN 1 END) AS lose
+
+FROM league
+where riot_name = '잘생긴정현이'
+and delete_yn  = 'N'
+and game_date >= DATE_TRUNC('month', CURRENT_TIMESTAMP)
+and game_date < DATE_TRUNC('month', CURRENT_TIMESTAMP) + INTERVAL '1 month'
+
+group by position;
+
+
+-- 모스트픽
+SELECT champ_name,count(champ_name) ,
+	    COUNT(CASE WHEN game_result = '승' THEN 1 END) AS win,
+	     COUNT(CASE WHEN game_result = '패' THEN 1 END) AS lose
+FROM league
+
+where riot_name = '잘생긴정현이'
+and delete_yn = 'N'
+group by champ_name, game_result
