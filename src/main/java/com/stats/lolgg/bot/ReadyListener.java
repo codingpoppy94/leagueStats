532f822745f7571bd52a1dea551459cce4325cd5
codingpoppy94 <60597797+codingpoppy94@users.noreply.github.com>
2024-06-01 00:07:50 +0900

    fix: 쓰지않는 코드정리

--- src/main/java/com/stats/lolgg/bot/ReadyListener.java
+++ src/main/java/com/stats/lolgg/bot/ReadyListener.java
@@ -88,18 +88,18 @@ public class ReadyListener extends ListenerAdapter {
                 * 팀 취소(추가작업필요)
                 */
 
-                if (message[0].equals("!ㅌㅊㅅ")) { 
-                    if(message.length > 1){
-                        String resultMessage = userManager.teamCancel(event, originMessage);
-                        if("error".equals(resultMessage)){
-                            sendMessage(channel,"존재하지않는 채널");
-                        }else {
-                            sendMessage(channel, resultMessage);
-                        }
-                    }else {
-                        sendErrorMessage(channel);
-                    }
-                }
+                // if (message[0].equals("!ㅌㅊㅅ")) { 
+                //     if(message.length > 1){
+                //         String resultMessage = userManager.teamCancel(event, originMessage);
+                //         if("error".equals(resultMessage)){
+                //             sendMessage(channel,"존재하지않는 채널");
+                //         }else {
+                //             sendMessage(channel, resultMessage);
+                //         }
+                //     }else {
+                //         sendErrorMessage(channel);
+                //     }
+                // }
         
                 /* 
                 * 목록 
