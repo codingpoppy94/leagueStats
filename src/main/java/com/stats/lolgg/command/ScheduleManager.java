f6367c5b3f496bfd7865dab32e228eecd1bf41ea
codingpoppy94 <60597797+codingpoppy94@users.noreply.github.com>
2024-06-01 19:43:23 +0900

    fix: 스케쥴 주석

--- src/main/java/com/stats/lolgg/command/ScheduleManager.java
+++ src/main/java/com/stats/lolgg/command/ScheduleManager.java
@@ -21,20 +21,20 @@ public class ScheduleManager {
     private final UserManager userManager;
     private final JDA jda;
 
-    // 매 오전 6시 마다
-    @Scheduled(cron="0 0 6 * * *")
-    public void clearUser(){
-        userManager.userClear();
-        String channelId = "1238766857289203773";
-        TextChannel channel = jda.getTextChannelById(channelId);
-        channel.sendMessage("```오전 6시 초기화 완료```").queue();
-    }
+    // // 매 오전 6시 마다
+    // @Scheduled(cron="0 0 6 * * *")
+    // public void clearUser(){
+    //     userManager.userClear();
+    //     String channelId = "1238766857289203773";
+    //     TextChannel channel = jda.getTextChannelById(channelId);
+    //     channel.sendMessage("```오전 6시 초기화 완료```").queue();
+    // }
 
-    // 매 오후 7시 체크 
-    @Scheduled(cron="0 0 19 * * *")
-    public void sendUserCheckMessage(){
-        String channelId = "1238766857289203773";
-        TextChannel channel = jda.getTextChannelById(channelId);
-        channel.sendMessage("```내전 체크해주세요. 9시 시작!```").queue();
-    }
+    // // 매 오후 7시 체크 
+    // @Scheduled(cron="0 0 19 * * *")
+    // public void sendUserCheckMessage(){
+    //     String channelId = "1238766857289203773";
+    //     TextChannel channel = jda.getTextChannelById(channelId);
+    //     channel.sendMessage("```내전 체크해주세요. 9시 시작!```").queue();
+    // }
 }
